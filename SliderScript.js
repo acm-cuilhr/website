9838442f84a4a969379e71a2501ba7e3010f8e40
Shan-e-Shafiq <shafiqshane6@gmail.com>
2024-08-06 19:46:55 +0500

    Completed Slider Section

---
+++ SliderScript.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
